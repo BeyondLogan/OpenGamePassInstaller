f9a3d98e12eef31ee6f20efef0d3bbc7739137bc
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-04 12:29:29 +0930

    Create SetUp2.sh

---
+++ SetUp2.sh
@@ -0,0 +1,43 @@
+sudo apt update
+
+sudo apt-get update
+
+sudo apt upgrade
+
+sudo apt-get upgrade
+
+sudo apt-get install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl
+
+sudo apt-get install libc6-i386
+
+sudo apt-get install libgcc1:i386
+
+sudo apt-get install libhttp-daemon-perl
+
+sudo apt-get install lib32gcc1
+
+sudo adduser ogpbot
+
+sudo usermod -a -G sudo ogpbot
+
+wget "https://github.com/OpenGamePanel/OGP-Agent-Linux/archive/master.zip"
+
+unzip master.zip
+
+cd OGP-Agent-Linux-master/
+
+sudo bash ./install.sh
+
+cd /var/www/html/themes/
+
+git clone https://github.com/hmrserver/Obsidian.git
+
+mv Obsidian/themes/Obsidian/* Obsidian/
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt update
+
+sudo apt upgrade
